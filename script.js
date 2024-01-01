0059ca4ab06da9ad0fce4835abadc605ece2b00d
sayudha <mazaya.studio@outlook.com>
2024-01-01 15:58:26 +0700

    Refactor confetti functionality

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import { startConfetti, stopConfetti, removeConfetti } from "./confetti.js";
+// import { startConfetti, stopConfetti, removeConfetti } from "./confetti.js";
 
 const playerScoreEl = document.getElementById("playerScore");
 const playerChoiceEl = document.getElementById("playerChoice");
@@ -37,8 +37,10 @@ function resetSelected() {
 	allGameIcons.forEach((icon) => {
 		icon.classList.remove("selected");
 	});
-	stopConfetti();
-	removeConfetti();
+	import("./confetti.js").then((module) => {
+		module.stopConfetti();
+		module.removeConfetti();
+	});
 }
 
 // Reset score & playerChoice/computerChoice
@@ -104,10 +106,12 @@ function updateScore(playerChoice) {
 	} else {
 		const choice = choices[playerChoice];
 		if (choice.defeats.indexOf(computerChoice) > -1) {
-			resultText.textContent = "You Won!";
-			startConfetti();
-			playerScoreNumber++;
-			playerScoreEl.textContent = playerScoreNumber;
+			import("./confetti.js").then((module) => {
+				resultText.textContent = "You Won!";
+				module.startConfetti();
+				playerScoreNumber++;
+				playerScoreEl.textContent = playerScoreNumber;
+			});
 		} else {
 			resultText.textContent = "You Lost!";
 			computerScoreNumber++;
